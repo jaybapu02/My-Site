36e0dead7785f6a112548c64c79beb959b96d59b
jaychandra <jaychandradasbapu@gmail.com>
2025-09-13 07:12:44 +0530

    debug=false

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vfz$bnrq_w989of=q!b)b*6io!6fto^llty#t4^x%sj1pg@3@7"
 # SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['jay-site.onrender.com', 'localhost', '127.0.0.1']
 
