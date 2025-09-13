8d5ef5053674651943f6731e14d4c6bd3b0edc9b
jaychandra <jaychandradasbapu@gmail.com>
2025-09-13 07:58:56 +0530

    **

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 APPEND_SLASH = False
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from mysite import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -30,3 +32,7 @@ urlpatterns = [
     path("skill/",views.skills,name='skills'),
     path('', views.home,name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
