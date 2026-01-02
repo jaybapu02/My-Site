e4e86adc273f116cfe1c194f7bc7093ee88efe63
jaychandra <jaychandradasbapu@gmail.com>
2026-01-02 22:49:17 +0530

    --

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,11 +134,14 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # APPEND_SLASH = False
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# import os
 
-EMAIL_HOST_USER = 'jaychandradasbapu05@gmail.com'
-EMAIL_HOST_PASSWORD = "zeqm dhsf lcno xqxs"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'True'
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
